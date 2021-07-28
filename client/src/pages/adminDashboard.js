3f16c8dda2a6ce785e9e65b2081e4e9ed09fcc9f
olinemazumder <olinemozumder@gmail.com>
2021-07-29 04:41:46 +0600

    Some issues

---
+++ client/src/pages/adminDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from "../components/adminDashboard/sidebar/Sidebar";
+
+
+const AdminDashboard = () => {
+
+  return (
+    <div className="container1">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default AdminDashboard;
---
+++ client/src/redux/actions/globalTypes.js
@@ -0,0 +1,25 @@
+export const GLOBALTYPES = {
+    AUTH: "AUTH",
+    ALERT: "ALERT",
+    THEME: 'THEME',
+    STATUS: 'STATUS',
+    MODAL: 'MODAL',
+    SOCKET: 'SOCKET',
+    ONLINE: 'ONLINE',
+    OFFLINE: 'OFFLINE',
+    CALL: 'CALL',
+    PEER: 'PEER',
+    USER_TYPE: "USER_TYPE",
+}
+
+export const EditData = (data, id, post) => {
+    const newData = data.map(item => 
+        (item._id === id ? post : item)
+    )
+    return newData;
+}
+
+export const DeleteData = (data, id) => {
+    const newData = data.filter(item => item._id !== id)
+    return newData;
+}
