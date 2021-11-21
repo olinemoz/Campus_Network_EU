04cf8c4973694f32a645c009fb857be48c791d90
olinemazumder <olinemozumder@gmail.com>
2021-11-22 01:20:02 +0600

    fixed discover

--- client/src/components/home/post_card/CardHeader.js
+++ client/src/components/home/post_card/CardHeader.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import Avatar from '../../Avatar'
-import { Link, useHistory } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import {Link, useHistory} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
 import moment from 'moment'
-import { GLOBALTYPES } from '../../../redux/actions/globalTypes'
-import { deletePost, reportPost } from '../../../redux/actions/postAction'
-import { BASE_URL } from '../../../utils/config'
+import {GLOBALTYPES} from '../../../redux/actions/globalTypes'
+import {deletePost, reportPost} from '../../../redux/actions/postAction'
+import {BASE_URL} from '../../../utils/config'
 
 const CardHeader = ({post}) => {
-    const { auth, socket } = useSelector(state => state)
+    const {auth, socket} = useSelector(state => state)
     const dispatch = useDispatch()
 
     const history = useHistory()
 
     const handleEditPost = () => {
-        dispatch({ type: GLOBALTYPES.STATUS, payload: {...post, onEdit: true}})
+        dispatch({type: GLOBALTYPES.STATUS, payload: {...post, onEdit: true}})
     }
 
     const handleDeletePost = () => {
-        if(window.confirm("Are you sure want to delete this post?")){
+        if (window.confirm("Are you sure want to delete this post?")) {
             dispatch(deletePost({post, auth, socket}))
             return history.push("/")
         }
@@ -35,18 +35,23 @@ const CardHeader = ({post}) => {
     return (
         <div className="card_header">
             <div className="d-flex">
-                <Avatar src={post.user.avatar} size="big-avatar" />
+                {
+                    post && post.user ?
+                        <>
+                            <Avatar src={post && post.user ? post.user.avatar : ''} size="big-avatar"/>
 
-                <div className="card_name">
-                    <h6 className="m-0">
-                        <Link to={`/profile/${post.user._id}`} className="text-dark">
-                            {post.user.username}
-                        </Link>
-                    </h6>
-                    <small className="text-muted">
-                        {moment(post.createdAt).fromNow()}
-                    </small>
-                </div>
+                            <div className="card_name">
+                                <h6 className="m-0">
+                                    <Link to={`/profile/${post.user._id}`} className="text-dark">
+                                        {post.user.username}
+                                    </Link>
+                                </h6>
+                                <small className="text-muted">
+                                    {moment(post.createdAt).fromNow()}
+                                </small>
+                            </div>
+                        </> : ""
+                }
             </div>
 
             <div className="nav-item dropdown">
@@ -61,7 +66,7 @@ const CardHeader = ({post}) => {
                             <div className="dropdown-item" onClick={handleEditPost}>
                                 <span className="material-icons">create</span> Edit Post
                             </div>
-                            <div className="dropdown-item" onClick={handleDeletePost} >
+                            <div className="dropdown-item" onClick={handleDeletePost}>
                                 <span className="material-icons">delete_outline</span> Remove Post
                             </div>
                         </>
